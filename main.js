8d9ee68d0fad43d9f11f1b0050b9149d5634cf15
MarcosMadera <CodingRules66@gmail.com>
2025-02-26 19:12:27 -0600

    update022625joe

--- main.js
+++ main.js
@@ -6,24 +6,24 @@ import { addFive } from "./utils.js";
 // function listen() {
 //     console.log("clicked");
 // }
-class User{
-    name;
-    hobbies;
-    constructor (name, hobbies) {
-      this.name = name;
-      this.hobbies = hobbies;
-    }
-    bio() {
-        this.hobbies.forEach((hobby) => {
-            const bio = `${this.name} likes ${hobby}`
-                    console.log(bio)
-                });
-    }
+class User {
+  name;
+  hobbies;
+  constructor(name, hobbies) {
+    this.name = name;
+    this.hobbies = hobbies;
+  }
+  bio() {
+    this.hobbies.forEach((hobby) => {
+      const bio = `${this.name} likes ${hobby}`;
+      console.log(bio);
+    });
+  }
 }
-const travis = new User("Travis", ['skiing', 'hiking', 'biking'])
-const mark = new User('Mark', ["coding", 'reading', 'swimming'])
-travis.bio()
-mark.bio()
+const travis = new User("Travis", ["skiing", "hiking", "biking"]);
+const mark = new User("Mark", ["coding", "reading", "swimming"]);
+travis.bio();
+mark.bio();
 // const user = {
 //     name: "Travis",
 //     hobbies: ['skiing', 'hiking', 'biking'],
@@ -33,7 +33,7 @@ mark.bio()
 //             console.log(bio)
 //         });
 //     }
-    
+
 // }
 // user.bio()
 
@@ -43,18 +43,38 @@ mark.bio()
 // function userInfo(user) {
 //     user.hobbies.forEach((hobby) => {
 // const phrase = `${user.name} likes ${hobby}`
-// console.log(phrase);  
+// console.log(phrase);
 //     })
 // }
-// userInfo(user)   
-
+// userInfo(user)
 
 // function calculateFunction(num1) {
 //     return addFive(num1)
 // }
-// let total = calculateFunction(200) 
+// let total = calculateFunction(200)
 // console.log(total)
 
 // calculateFunction(200)
 
+class Car {
+  brand;
 
+  constructor(brand) {
+    this.brand = brand;
+  }
+  present() {
+    return `This car is a ${this.brand}`;
+  }
+}
+class Model extends Car {
+  name;
+  constructor(brand, name) {
+    super(brand);
+    this.name = name;
+  }
+  show() {
+    return `${this.present()} and the model is: ${this.name}`;
+  }
+}
+const myCar = new Model("Toyota", "Corolla");
+console.log(myCar.show());
