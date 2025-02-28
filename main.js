3b91078eb4b7badf5b25e82c7b3bb34c2d04c760
MarcosMadera <CodingRules66@gmail.com>
2025-02-27 19:26:38 -0600

    learningJSONlocalstorage

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 // let btn = document.querySelector("#clickBtn");
 // btn.addEventListener("click",listen)
 
-import { addFive } from "./utils.js";
+// import { addFive } from "./utils.js";
 
 // function listen() {
 //     console.log("clicked");
 // }
-class User {
-  name;
-  hobbies;
-  constructor(name, hobbies) {
-    this.name = name;
-    this.hobbies = hobbies;
-  }
-  bio() {
-    this.hobbies.forEach((hobby) => {
-      const bio = `${this.name} likes ${hobby}`;
-      console.log(bio);
-    });
-  }
-}
-const travis = new User("Travis", ["skiing", "hiking", "biking"]);
-const mark = new User("Mark", ["coding", "reading", "swimming"]);
-travis.bio();
-mark.bio();
+// class User {
+//   name;
+//   hobbies;
+//   constructor(name, hobbies) {
+//     this.name = name;
+//     this.hobbies = hobbies;
+//   }
+//   bio() {
+//     this.hobbies.forEach((hobby) => {
+//       const bio = `${this.name} likes ${hobby}`;
+//       console.log(bio);
+//     });
+//   }
+// }
+// const travis = new User("Travis", ["skiing", "hiking", "biking"]);
+// const mark = new User("Mark", ["coding", "reading", "swimming"]);
+// travis.bio();
+// mark.bio();
 // const user = {
 //     name: "Travis",
 //     hobbies: ['skiing', 'hiking', 'biking'],
@@ -56,25 +56,65 @@ mark.bio();
 
 // calculateFunction(200)
 
-class Car {
-  brand;
+// class Car {
+//   brand;
 
-  constructor(brand) {
-    this.brand = brand;
-  }
-  present() {
-    return `This car is a ${this.brand}`;
-  }
-}
-class Model extends Car {
-  name;
-  constructor(brand, name) {
-    super(brand);
-    this.name = name;
-  }
-  show() {
-    return `${this.present()} and the model is: ${this.name}`;
-  }
-}
-const myCar = new Model("Toyota", "Corolla");
-console.log(myCar.show());
+//   constructor(brand) {
+//     this.brand = brand;
+//   }
+//   present() {
+//     return `This car is a ${this.brand}`;
+//   }
+// }
+// class Model extends Car {
+//   name;
+//   constructor(brand, name) {
+//     super(brand);
+//     this.name = name;
+//   }
+//   show() {
+//     return `${this.present()} and the model is: ${this.name}`;
+//   }
+// }
+// const myCar = new Model("Toyota", "Corolla");
+// console.log(myCar.show());
+
+
+// const user = {
+//    name: 'Mark'
+   
+// }
+// console.log(user)
+// let jsonUser = JSON.stringify(user)
+// console.log(jsonUser)
+
+// // // function hello() {
+// //     return "Hello"
+// // }
+// // console.log(hello)
+
+// // let jsonHello = JSON.stringify(hello)
+// // console.log(jsonHello)
+// console.log(jsonUser.name)
+
+// let parsedUser = JSON.parse(jsonUser)
+// // console.log(parsedUser.name)
+// // window.localStorage.setItem("name", "Mark")
+
+// // let item = window.localStorage.getItem('name')  
+// // console.log(item)
+
+// let items = ['milk', 'apples', 'grapes'];
+// console.log(items)
+// let necessaryItems = JSON.stringify(items)
+// console.log(necessaryItems)
+// window.localStorage.setItem('items', necessaryItems);
+
+let groceryList = [];
+
+let groceries = window.localStorage.getItem('items')
+
+let parsedGroceries = JSON.parse(groceries)
+// console.log(parsedGroceries)
+groceryList = parsedGroceries
+console.log(groceryList)
